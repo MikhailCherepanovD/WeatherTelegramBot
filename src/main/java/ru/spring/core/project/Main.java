f812522349dada946e38d2330f396ea54ec9b056
KevGen <kev210204@gmail.com>
2024-05-09 00:08:58 +0300

    write test connection to tg bot

--- src/main/java/ru/spring/core/project/Main.java
+++ src/main/java/ru/spring/core/project/Main.java
@@ -1,7 +1,19 @@
-package ru.kevgen;
+package ru.spring.core.project;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.spring.core.project.Bot.Bot;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Bot());
+
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
